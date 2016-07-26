816d897c682bcc21709c9fbda2927ae7616cf74e
saplmm <1046439010@qq.com>
2016-07-27 00:43:58 +0800

    静态文件搞定了

--- server.go
+++ server.go
@@ -1,11 +1,24 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
 
 func main() {
 	m := martini.Classic()
+
+	//中间件
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
+
 	m.Get("/", func() string {
 		return "Hello world!"
+	})
+
+	m.Get("/render", func(r render.Render) {
+		r.HTML(200, "hello", "jeremy")
 	})
 	m.Run()
 }
